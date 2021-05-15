34cfe93a57c6c731f5b2f3bb73bbd118eb1f1c62
SHUKLA123 <32232851+SHUKLA123@users.noreply.github.com>
2021-05-16 01:43:04 +0530

    Update Question-1.py

--- Question-1.py
+++ Question-1.py
@@ -8,7 +8,7 @@ msg = EmailMessage()
 
 msg['Subject'] = str(input('Enter subject of the email : '))
 msg.set_content(str(input('Enter body of the email : ')))
-msg['From'] = "abcdef7700000000@gmail.com"
+msg['From'] = "Write_Your_Email_Id" #Example abcdef7700000000@gmail.com
 msg['To'] = str(input('Enter Recipient of the email : '))
 
 # Send the message via our own SMTP server.
